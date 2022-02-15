1b157cb9ab34b24d7c33bbb86b9fad29cc0821f3
hemapriya <hemapriyasampath@gmail.com>
2022-02-15 14:04:24 -0500

    destructuring arguments

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import TableHeader from "../common/tableHeader";
 import TableBody from "../common/tableBody";
 
-const Table = (props) => {
-  const { columns, sortColumn, onSort, data } = props;
+const Table = ({ columns, sortColumn, onSort, data }) => {
   return (
     <div>
       <table className="table">
